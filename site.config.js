ca5facd62d379d3ae033043604d4c3b37b6900f1
HyoTaek <kht1790@gmail.com>
2025-04-16 16:43:50 +0900

    [Config] 블로그 사이트 설정 변경

--- site.config.js
+++ site.config.js
@@ -1,32 +1,36 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "DIN-STUDIO",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
+    role: "iOS Developer",
+    bio: "협업과 문서화를 중시하는 개발자입니다.",
+    email: "kht1790@naver.com",
+    linkedin: "",
     github: "morethanmin",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `향모아`,
+      href: "https://github.com/HMOAA/HMOA_iOS",
     },
+    {
+      name: `밥점줘`,
+      href: "https://github.com/INU-BJJ/bjj-iOS",
+    }
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    title: "JustDinLog",
+    description: "Welcome to iOS world!",
+    scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://just-din-log.vercel.app",
+  since: 2025, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
